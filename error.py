e80ccb75ec1cb14589146ce70668c87b4e9c6245
karbag89 <karbag89@rambler.ru>
2021-08-11 19:55:58 +0400

    Documented and adjusted the 'error.py' Python file.

--- error.py
+++ error.py
@@ -1,8 +1,32 @@
+# ----------------------------------------------------------------------------
+# Python library dependency
+# ----------------------------------------------------------------------------
+
 from flask import json
 
 
+# ----------------------------------------------------------------------------
+# Description :
+# The Error class getting error data (error_code, error_message), creating and
+# returning error log message in JSON body.
+# ----------------------------------------------------------------------------
 class Error:
-    def errorMessage(error_code, error_message) -> json:
+    '''Takes error code and error message values, returns error log message.'''
+    # -------------------------------------------------------------------------
+    # Description :
+    # This function getting error code and error message data, creating and
+    # returning error log message in JSON body.
+    #
+    # Parameters :
+    #     error_code      Error code value.
+    #     error_message   Error message text value.
+    #
+    # Return :
+    #     data            Error log message in JSON format.
+    #
+    # -------------------------------------------------------------------------
+    def errorMessage(self, error_code, error_message) -> json:
+        '''Creating error log message in JSON body.'''
         data = {"code": error_code,
                 "message": error_message}
         return data
